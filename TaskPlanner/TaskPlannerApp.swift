58e7b380507a0878b5e79623d2d79029c9e68a56
YahefuYusufu <parguzatt@gmail.com>
2023-11-26 12:49:57 +0100

    Initial Commit

---
+++ TaskPlanner/TaskPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskPlannerApp.swift
+//  TaskPlanner
+//
+//  Created by yusufyakuf on 2023-11-26.
+//
+
+import SwiftUI
+
+@main
+struct TaskPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

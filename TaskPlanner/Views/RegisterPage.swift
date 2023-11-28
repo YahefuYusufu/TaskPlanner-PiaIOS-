1279b89f993d3b9b0267c14cef6db3d42daecc7b
YahefuYusufu <parguzatt@gmail.com>
2023-11-28 17:24:54 +0100

    login page has dine , but get some issue (UIViewAlertForUnsatisfiableConstraints) fill fix it next time

---
+++ TaskPlanner/Views/RegisterPage.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterPage.swift
+//  TaskPlanner
+//
+//  Created by yusufyakuf on 2023-11-28.
+//
+
+import SwiftUI
+
+struct RegisterPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterPage()
+}
